33321380dd590cb4429180df70537e11a136a103
Shallyeneje <Shalomenje2@gmail.com>
2024-07-18 09:59:39 +0100

    new commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="hro ">
       <div className="hero-section container">
-        <div className="container row ">
+        <div className=" row ">
           <div className="d-flex justify-content-center align-items-center col-12">
             <div className="mt-4">
               <h1 className="hero-text">
@@ -17,8 +17,8 @@ const Hero = () => {
               <h1 className="hero-text">for academic growth</h1>
             </div>
           </div>
-          <div className=" heroimg justify-content-center container col-12 ">
-          <img src={heroimg} alt="heroimgage" />
+          <div className="  col-12 ">
+          <img  className="heroimg " src={heroimg} alt="heroimgage" />
         </div>
         </div>
       </div>
--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,40 +1,90 @@
-import React from 'react';
-import {  NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 import "./Nav.css";
-
 
 const Navbar = () => {
   return (
-    
     <nav className="  navbar navbar-expand-lg  ">
-      <div className='dash '> StudyBoosta</div>
-      
-      
-      <div className=" collapse navbar-collapse justify-content-center" id="navbarNav">
+      <div className="dash "> StudyBoosta</div>
+
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div
+        className=" collapse navbar-collapse justify-content-center"
+        id="navbarNav"
+      >
         <ul className=" navbarnav navbar-expand d-flex ">
           <li className="nav-link p-1">
-            <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'nav-link')} to={"/"}>Home</NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "active nav-link" : "nav-link"
+              }
+              to={"/"}
+            >
+              Home
+            </NavLink>
           </li>
           <li className="nav-link p-1">
-            <NavLink to={"/courses"} className={({ isActive }) => (isActive ? 'active nav-link' : 'nav-link')}>Courses</NavLink>
+            <NavLink
+              to={"/courses"}
+              className={({ isActive }) =>
+                isActive ? "active nav-link" : "nav-link"
+              }
+            >
+              Courses
+            </NavLink>
           </li>
           <li className="nav-link p-1">
-            <NavLink to={"/chatbot"} className={({ isActive }) => (isActive ? 'active nav-link' : 'nav-link')}>Chatbot</NavLink>
+            <NavLink
+              to={"/chatbot"}
+              className={({ isActive }) =>
+                isActive ? "active nav-link" : "nav-link"
+              }
+            >
+              Chatbot
+            </NavLink>
           </li>
           <li className="nav-link p-1">
-            <NavLink to={"/Scholarship"} className={({ isActive }) => (isActive ? 'active nav-link' : 'nav-link')} >Scholarships</NavLink>
+            <NavLink
+              to={"/Scholarship"}
+              className={({ isActive }) =>
+                isActive ? "active nav-link" : "nav-link"
+              }
+            >
+              Scholarships
+            </NavLink>
           </li>
           <li className="nav-link p-1">
-            <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'nav-link')} to={"/security"}>Security</NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "active nav-link" : "nav-link"
+              }
+              to={"/security"}
+            >
+              Security
+            </NavLink>
           </li>
         </ul>
       </div>
       <div>
-        <NavLink to={"/Register"} className={"btn btn-warning btn-get-started ml-auto"}>Get Started</NavLink>
+        <NavLink
+          to={"/Register"}
+          className={"btn btn-warning btn-get-started ml-auto"}
+        >
+          Get Started
+        </NavLink>
       </div>
     </nav>
-    
   );
-}
+};
 
 export default Navbar;
